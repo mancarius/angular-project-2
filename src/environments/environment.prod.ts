8556e28c70f51954962ea9665666f9f6a1ecb58f
Mattia <75211269+mancarius@users.noreply.github.com>
2022-08-05 17:41:45 +0200

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  pexelsApiKey: "563492ad6f9170000100000113902aef15b6476cbdd074c452b70b1e",
-};
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
